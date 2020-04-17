93e7509a6855e4a1fb78cbe46d1401c3f10280b8
hrupesh <rcashok0@gmail.com>
2020-04-17 19:15:08 +0530

    Now A PWA

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{html,jpg,gif,css,txt,scss,eot,svg,ttf,woff,pdf,woff2,otf,jpeg,psd,png,jpf,PNG,js,json}"
+  ],
+  "swDest": "build/sw.js"
+};
